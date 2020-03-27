96a4a1e078d0b6ad1613d82f42af9c1293d3f8a3
Duje Majić <duje.majic55@gmail.com>
2020-03-27 14:44:45 +0100

    finished basic todo functionality

--- src/components/Todo/TodoInput.jsx
+++ src/components/Todo/TodoInput.jsx
@@ -2,9 +2,10 @@ import React, { Component } from "react";
 class TodoInput extends Component {
   state = {};
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
       <div className="card card-body my-3">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="input-group">
             <div className="input-group-prepend">
               <div className="input-group-text bg-primary text-white">
@@ -15,10 +16,19 @@ class TodoInput extends Component {
               type="text"
               className="form-control text-capitalize"
               placeholder="add a todo"
+              value={item}
+              onChange={handleChange}
             />
           </div>
-          <button type="submit" className="btn btn-block btn-primary mt-3">
-            add item
+          <button
+            type="submit"
+            className={
+              editItem
+                ? "btn btn-block btn-success mt-3"
+                : "btn btn-block btn-primary mt-3"
+            }
+          >
+            {editItem ? "edit item" : "add item"}
           </button>
         </form>
       </div>
--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -4,13 +4,25 @@ import TodoItem from "./TodoItem";
 class TodoList extends Component {
   state = {};
   render() {
+    const { items, clearList, handleDelete, handleEdit } = this.props;
     return (
       <ul className="list-group my-5">
         <h3 className="text-capitalize text-center">todo list</h3>
-        <TodoItem></TodoItem>
+        {items.map(item => {
+          return (
+            <TodoItem
+              key={item.id}
+              title={item.item}
+              handleDelete={() => handleDelete(item.id)}
+              handleEdit={() => handleEdit(item.id)}
+            />
+          );
+        })}
+
         <button
           type="button"
           className="btn btn-danger btn-block text-capitalize mt-5"
+          onClick={clearList}
         >
           clear list
         </button>
